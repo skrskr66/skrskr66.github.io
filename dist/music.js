87777e3291010ad890875f1c37f692d2014db833
skrskr66 <847020588@qq.com>
2018-11-05 01:44:55 +0800

    Site updated: 2018-11-05 01:44:53

---
+++ dist/music.js
@@ -0,0 +1,12 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "年少有为",
+        artist: '李荣浩',
+        url: 'http://www.ytmp3.cn/down/50491.mp3'
+      }
+    ]
+});
