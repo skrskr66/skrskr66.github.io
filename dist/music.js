07a296fd690cbed52a278f9503aa54067bcd2cbd
skrskr66 <847020588@qq.com>
2018-11-06 00:07:38 +0800

    Site updated: 2018-11-06 00:07:36

--- dist/music.js
+++ dist/music.js
@@ -1,9 +1,9 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: false,
+    autoplay: true,
     audio: [
-      {
+      { 
         name: "unnatural（女生翻唱版）",
         artist: '迷津玄师',
         url: 'http://www.ytmp3.cn/down/53497.mp3'，
