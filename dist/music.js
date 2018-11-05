27f16f0975e718765313cf19a6612e11a1bb9e03
skrskr66 <847020588@qq.com>
2018-11-05 12:38:51 +0800

    Site updated: 2018-11-05 12:38:44

--- dist/music.js
+++ dist/music.js
@@ -6,7 +6,17 @@ const ap = new APlayer({
       {
         name: "年少有为",
         artist: '李荣浩',
-        url: 'http://www.ytmp3.cn/down/50491.mp3'
+        url: 'http://www.ytmp3.cn/down/50491.mp3'，
+      }，
+      {
+        name: "unnatural（女生翻唱版）",
+        artist: '迷津玄师',
+        url: 'http://www.ytmp3.cn/down/53497.mp3'，
+      }，
+      {
+        name: "狂恋你",
+        artist: '沈以城',
+        url: 'http://www.ytmp3.cn/down/49604.mp3'，
       }
     ]
 });
